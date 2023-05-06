78e63a1c2c24157244cb59a90c329f831fb12298
santomi3112 <adiprasetio007@gmail.com>
2023-05-06 15:47:22 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const fs = require('fs').promises
+
+const apiKey = '5DCSCT4M43SKCBYFXG46VSG2YCRU7M25MH';
+const contractAddress = '0x378E1BE15BE6d6D1f23Cfe7090b6a77660dBF14d';
+const walletsFile = 'wallets.txt';
+
+async function getWallets() {
+  const data = await fs.readFile(walletsFile, 'utf-8');
+  const wallets = data.split('\n').filter((wallet) => wallet.trim() !== '');
+  return wallets;
+}
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function checkBalances() {
+  const wallets = await getWallets();
+
+  for (const wallet of wallets) {
+    const url = `https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${contractAddress}&address=${wallet}&tag=latest&apikey=${apiKey}`;
+    const response = await axios.get(url);
+    const balance = parseFloat(response.data.result) / Math.pow(10, 18); // Assuming the token has 18 decimals
+    console.log(`Balance for wallet ${wallet}: ${balance}`);
+
+    await delay(1000); // 1 sec
+  }
+}
+
+checkBalances();
